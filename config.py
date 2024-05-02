fd6961631061e9de19ba24522918f32f246c56f3
Irina Luchkova <shtejn@gmail.com>
2024-05-02 16:44:26 +0300

    1

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+context = os.getenv('context', 'bstack')
+bstack_userName = os.getenv('bstack_userName', 'bsuser_NYkOrt')
+bstack_accessKey = os.getenv('bstack_accessKey', 'eVgSnKA52PSNxW2o6Yox')
